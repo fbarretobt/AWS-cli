056ff60c8f4c6b383218aeb68c5e632e05ca11f9
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-18 16:16:42 -0500

    add aguments for different acc

--- snapshotfinder_infra.sh
+++ snapshotfinder_infra.sh
@@ -1,34 +1,70 @@
 #!/bin/sh
 
-Snapshot=$(aws ec2 describe-snapshots --owner-id self --filters Name=encrypted,Values=false --output text | awk '{print $10}')
 
-
-
-for i in $Snapshot
-do 
-
-
-	echo "=========================================================================================================="
-	echo "                                                                                                     "
-	echo "                                                                                                      "
-	echo "     AMI information based on Snapshot $i description                                                 "
-	echo "                                                                                                      "
-	echo "                                                                                                      "
-	echo "=========================================================================================================="
+infra(){
 	
+	Snapshot=$(aws ec2 describe-snapshots --owner-id self --filters Name=encrypted,Values=false --output text | awk '{print $10}')
 
-	snapinfo=$(aws ec2 describe-snapshots --owner-ids self --snapshot-ids $i)
+
+	for i in $Snapshot
+	do 
 
 
-	snap_id=$(jq -r '.[] | .[] | .SnapshotId' <<< "$snapinfo")
-	encryption=$(jq -r '.[] | .[] | .Encrypted' <<< "$snapinfo")
-	ami=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $5}')
+		echo "=========================================================================================================="
+		echo "                                                                                                     "
+		echo "                                                                                                      "
+		echo "     AMI information based on Snapshot $i description                                                 "
+		echo "                                                                                                      "
+		echo "                                                                                                      "
+		echo "=========================================================================================================="
+		
 
-	
-	echo "===== AMI info"
-	aws ec2 describe-images --image-ids ami-022b4ada5cc036418 --query 'Images[*].{ EBS_ecryption:BlockDeviceMappings[*].Ebs.Encrypted, DeleteOnTermination:BlockDeviceMappings[*].Ebs.DeleteOnTermination, Image_Name: Name, State:State}'   --output 
-table
+		snapinfo=$(aws ec2 describe-snapshots --owner-ids self --snapshot-ids $i)
 
-	echo "===== Snapshot info"
-	aws ec2 describe-snapshots --owner-ids self --snapshot-ids $snap_id --output table
-done 
+
+		snap_id=$(jq -r '.[] | .[] | .SnapshotId' <<< "$snapinfo")
+		encryption=$(jq -r '.[] | .[] | .Encrypted' <<< "$snapinfo")
+		ami=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $5}')
+
+		
+		echo "===== AMI info"
+		aws ec2 describe-images --image-ids ami-022b4ada5cc036418 --query 'Images[*].{ EBS_ecryption:BlockDeviceMappings[*].Ebs.Encrypted, DeleteOnTermination:BlockDeviceMappings[*].Ebs.DeleteOnTermination, Image_Name: Name, State:State}'   --output 
+	table
+
+		echo "===== Snapshot info"
+		aws ec2 describe-snapshots --owner-ids self --snapshot-ids $snap_id --output table
+	done
+
+}
+
+
+
+rs(){
+	echo"rs"
+}
+
+
+pra(){
+	echo"PRA"
+}
+
+
+
+while getopts 'ipr:' OPTION; do
+  case "$OPTION" in
+    i)
+      infra()
+      ;;
+    p)
+      pra()
+      ;;
+    r)
+      rs()
+      ;;
+    ?)
+      echo "script usage: $(basename \$0) [-i] [-i] [-r]" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
