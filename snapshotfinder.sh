0f3cb29e9ae1b8259b4603ea4df0d32c71607574
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-18 10:37:49 -0500

    Updated  display

--- snapshotfinder.sh
+++ snapshotfinder.sh
@@ -2,8 +2,16 @@
 
 AMI=$(aws ec2 describe-snapshots --owner-id self --filters Name=encrypted,Values=false --output text | awk '{print $6}')
 
-for i in AMI
+for i in $AMI
 do 
+	aws ec2 describe-images --image-ids $i  --query 'Images[*].{Image_Name: Name, State:State}'   --output table 
+	echo "=========================================================================================================="
+	echo "||                                                                                                      ||"
+	echo "||                                                                                                      ||"
+	echo "||     AMI information based on Snapshot description                                                    ||"
+	echo "||                                                                                                      ||"
+	echo "||                                                                                                      ||"
+	echo "=========================================================================================================="
 	aws ec2 describe-images --image-ids $i  --query 'Images[*].{Image_Name: Name, State:State}'   --output table 
 done 
 
