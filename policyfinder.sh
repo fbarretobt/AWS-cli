32fff3c105e52fe29ff64a745052294978432f6e
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-19 12:47:03 -0500

    finder for policy of unencrypted snapshots

---
+++ policyfinder.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+
+for REGION in $(aws ec2 describe-regions --output text --query 'Regions[].[RegionName]')
+	do 
+		echo $REGION
+
+		Snapshot=$(aws ec2 describe-snapshots --owner-id self  --query 'Snapshots[].SnapshotId' --filters Name=encrypted,Values=false --output text --region $REGION )
+
+
+		for i in $Snapshot
+		do 
+
+
+			echo "=========================================================================================================="
+			echo "                                                                                                     "
+			echo "                                                                                                      "
+			echo "     AMI information based on Snapshot $i description                                                 "
+			echo "                                                                                                      "
+			echo "                                                                                                      "
+			echo "=========================================================================================================="
+			
+
+			snapinfo=$(aws ec2 describe-snapshots --owner-ids self --snapshot-ids $i --region $REGION)
+
+
+			snap_id=$(jq -r '.[] | .[] | .SnapshotId' <<< "$snapinfo")
+			encryption=$(jq -r '.[] | .[] | .Encrypted' <<< "$snapinfo")
+			ami=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $5}')
+			policy=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $4}')
+
+
+			if [[ "$policy" =~ .*"policy-".* ]]; then
+				echo "===== Policy Info"
+				policy_info=$(aws dlm get-lifecycle-policy --policy-id $policy --output json --region $REGION) 
+				
+				
+				description=$(jq -r '.Policy.Description' <<< "$policy_info")
+				echo "DEscription : " $description
+				policy_id=$(jq -r '.Policy.PolicyId' <<< "$policy_info")
+				echo "Policy ID : " $policy_id
+				state=$(jq -r '.Policy.State' <<< "$policy_info")
+				echo "State : " $state
+            fi
+
+			echo "===== Snapshot info"
+			aws ec2 describe-snapshots --owner-ids self --snapshot-ids $snap_id --output table --region $REGION
+
+		done
+
+
+	done
