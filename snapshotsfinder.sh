cf25cad5710ca552415174a38138f3ebae1a6308
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-20 15:38:57 -0500

    update

--- snapshotsfinder.sh
+++ snapshotsfinder.sh
@@ -2,26 +2,26 @@
 
 
 for REGION in $(aws ec2 describe-regions --output text --query 'Regions[].[RegionName]')
-	do 
+do 
 
     Snapshot=$(aws ec2 describe-snapshots --owner-id self  --query 'Snapshots[].SnapshotId' --filters Name=encrypted,Values=false --output text )
 
-			echo "=========================================================================================================="
-			echo "                                                                                                     "
-			echo "                                                                                                      "
-			echo "     AMI information based on Snapshot $i description                                                 "
-			echo "                                                                                                      "
-			echo "                                                                                                      "
-			echo "=========================================================================================================="
+    echo "=========================================================================================================="
+    echo "   "                                                                                                  
+    echo "   "                                                                                                   
+    echo "     AMI information based on Snapshot $i description  "                                                
+    echo "   "                                                                                                  
+    echo "   "                                                                                                    
+    echo "=========================================================================================================="
 			
     snapinfo=$(aws ec2 describe-snapshots --owner-ids self --snapshot-ids $i)
 
 
-		snap_id=$(jq -r '.[] | .[] | .SnapshotId' <<< "$snapinfo")
-		encryption=$(jq -r '.[] | .[] | .Encrypted' <<< "$snapinfo")
-		ami=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $5}')
-		policy=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $4}')
-        name=$(jq -r '.[] | .[] | .[] | .Name' <<< "$snapinfo" )
+    snap_id=$(jq -r '.[] | .[] | .SnapshotId' <<< "$snapinfo")
+    encryption=$(jq -r '.[] | .[] | .Encrypted' <<< "$snapinfo")
+    ami=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $5}')
+    policy=$(jq -r '.[] | .[] | .Description' <<< "$snapinfo" | awk '{print $4}')
+    name=$(jq -r '.[] | .[] | .[] | .Name' <<< "$snapinfo" )
 
-        echo "Name : " $name
+    echo "Name : " $name
 done
