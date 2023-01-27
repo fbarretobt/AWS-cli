2d34073fbe7518ad33161eb63b782636b30682a4
Felipe Barreto <fbarreto@beyondtrust.com>
2023-01-27 17:28:29 -0500

    updated py

--- snapshot_xray.py
+++ snapshot_xray.py
@@ -3,8 +3,12 @@ import boto3
 
 ec2 = boto3.client('ec2')
 
-regions = ec2.describe_regions()
+regions_list = ec2.describe_regions()
 
-print( "Regionws = " + regions)
+#print(regions)
+ 
+regions = regions_list["Regions"]
 
+for region in regions: 
+    print(region['RegionName'])
 
