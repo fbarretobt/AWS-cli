ff23d2542cf4ac5cd0461776818fcf970f4fafa1
Felipe Barreto <fbarreto@beyondtrust.com>
2023-01-27 17:46:02 -0500

    Update

--- snapshot_xray.py
+++ snapshot_xray.py
@@ -1,14 +1,26 @@
 import boto3
+import json
+import sys
+from pprint import pprint
+from datetime import datetime, timezone
 
 
-ec2 = boto3.client('ec2')
-
-regions_list = ec2.describe_regions()
+regions_list = boto3.client.ec2.describe_regions()
 
 #print(regions)
  
 regions = regions_list["Regions"]
 
 for region in regions: 
-    print(region['RegionName'])
 
+    regionA = print(region['RegionName'])
+
+    ec2 = boto3.client(ec2, regionA)
+    resp = ec2.describe_instances()
+    resp_describe_snapshots = ec2.describe_snapshots(OwnerIds=['*******'])
+    snapshot =  resp_describe_snapshots['Snapshots']
+    snapshots = [''];
+    for snapshotIdList in resp_describe_snapshots['Snapshots']:
+        snapshots.append(snapshotIdList.get('SnapshotId'))
+    for id in snapshots:
+        print(id)
