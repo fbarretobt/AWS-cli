f0ba3fba1a405b707f3f5a4c23374f06b2be08f8
Felipe Barreto <fbarreto@beyondtrust.com>
2023-01-30 10:49:32 -0500

    updated

--- snapshot_xray.py
+++ snapshot_xray.py
@@ -12,15 +12,27 @@ regions_list = e.describe_regions()
 regions = regions_list["Regions"]
 
 for region in regions: 
-
+    print ("+")
+    print ("+")
+    print ("+")
+    print ("++++++++++++++++++++++++++++++++++++++++")
     regionA = print(region['RegionName'])
+    print ("++++++++++++++++++++++++++++++++++++++++")
+    print ("+")
 
-    ec2 = boto3.client(ec2, regionA)
-    resp = ec2.describe_instances()
-    resp_describe_snapshots = ec2.describe_snapshots(OwnerIds=['self'])
-    snapshot =  resp_describe_snapshots['Snapshots']
-    snapshots = [''];
-    for snapshotIdList in resp_describe_snapshots['Snapshots']:
-        snapshots.append(snapshotIdList.get('SnapshotId'))
-    for id in snapshots:
-        print(id)
+    ec2 = boto3.client('ec2', regionA)
+    snapshot_response = ec2.describe_snapshots(OwnerIds=['self'])
+
+    for snapshot in snapshot_response['Snapshots']:
+        print(snapshot['SnapshotId'])
+        print(snapshot['VolumeId'])
+        print(snapshot['VolumeSize'])
+        print(snapshot['StartTime'])
+        days_old = (datetime.now(timezone.utc) - snapshot['StartTime']).days
+        print(days_old)
+
+        volume_response = ec2.describe_volumes(VolumeIds=[snapshot['VolumeId']])
+        volume = volume_response['Volumes'][0]
+        print(volume['VolumeType'])
+        for attachment in volume['Attachments']:
+            print(attachment['InstanceId'])
