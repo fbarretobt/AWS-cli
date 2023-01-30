ba76a20b4bba587fc0a58ae1a32d24b3459fee9a
Felipe Barreto <fbarreto@beyondtrust.com>
2023-01-30 12:13:38 -0500

    update

--- snapshot_xray.py
+++ snapshot_xray.py
@@ -34,26 +34,10 @@ for region in regions:
             print ("+++++++++++++++++++++++++++")
             print ("+")
             print("Snapshot Id : " + snapshot['SnapshotId'])
-            print("Volume Id :" +snapshot['VolumeId'])
-            print("Volume Size :", snapshot['VolumeSize'])
             print("Creation date :", snapshot['StartTime'])
             print("Snapshot is ", days_old, "days old")
 
-            print(snapshot['Tags'])
 
-            try:
-                volume_response = ec2.describe_volumes(VolumeIds=[snapshot['VolumeId']])
-                volume = volume_response['Volumes'][0]
-                print("Volume Type :" +volume['VolumeType'])
-                for attachment in volume['Attachments']:
-                    print("Instance ID :" + attachment['InstanceId'])
-                    print ("+")
-                    print ("+++++++++++++++++++++++++++")
-            except botocore.exceptions.ClientError as error:
-                if error.response['Error']['Code'] == 'InvalidVolume.NotFound':
-                    print("Volume not found ", volume['VolumeId'] )
-                else: # Unknown exception
-                    print(error.response)
 
         else :
 
