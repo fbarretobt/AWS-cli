7398aa4ba78c3824720f36e26e2d06b2cd3fdf2d
Felipe Barreto <fbarreto@beyondtrust.com>
2023-01-30 11:40:43 -0500

    update

--- snapshot_xray.py
+++ snapshot_xray.py
@@ -1,3 +1,4 @@
+from tracemalloc import Snapshot
 import boto3
 import json
 import sys
@@ -25,24 +26,35 @@ for region in regions:
     snapshot_response = ec2.describe_snapshots(OwnerIds=['self'])
 
     for snapshot in snapshot_response['Snapshots']:
-        print ("+++++++++++++++++++++++++++")
-        print ("+")
-        print("Snapshot Id : " + snapshot['SnapshotId'])
-        print("Volume Id :" +snapshot['VolumeId'])
-        print("Volume Size :", snapshot['VolumeSize'])
-        print("Creation date :", snapshot['StartTime'])
+
         days_old = (datetime.now(timezone.utc) - snapshot['StartTime']).days
-        print("Snapshot is ", days_old, "days old")
-        try:
-            volume_response = ec2.describe_volumes(VolumeIds=[snapshot['VolumeId']])
-            volume = volume_response['Volumes'][0]
-            print("Volume Type :" +volume['VolumeType'])
-            for attachment in volume['Attachments']:
-                print("Instance ID :" + attachment['InstanceId'])
-                print ("+")
-                print ("+++++++++++++++++++++++++++")
-        except botocore.exceptions.ClientError as error:
-            if error.response['Error']['Code'] == 'InvalidVolume.NotFound':
-                print("Volume not found ", volume['VolumeId'] )
-            else: # Unknown exception
-                print(error.response)
+
+
+        if days_old >= 30:
+            print ("+++++++++++++++++++++++++++")
+            print ("+")
+            print("Snapshot Id : " + snapshot['SnapshotId'])
+            print("Volume Id :" +snapshot['VolumeId'])
+            print("Volume Size :", snapshot['VolumeSize'])
+            print("Creation date :", snapshot['StartTime'])
+            print("Snapshot is ", days_old, "days old")
+
+            print(snapshot['Tags'])
+
+            try:
+                volume_response = ec2.describe_volumes(VolumeIds=[snapshot['VolumeId']])
+                volume = volume_response['Volumes'][0]
+                print("Volume Type :" +volume['VolumeType'])
+                for attachment in volume['Attachments']:
+                    print("Instance ID :" + attachment['InstanceId'])
+                    print ("+")
+                    print ("+++++++++++++++++++++++++++")
+            except botocore.exceptions.ClientError as error:
+                if error.response['Error']['Code'] == 'InvalidVolume.NotFound':
+                    print("Volume not found ", volume['VolumeId'] )
+                else: # Unknown exception
+                    print(error.response)
+
+        else :
+
+            continue
