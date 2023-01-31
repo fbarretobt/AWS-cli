da4926568c4725239bf54557f794d770fb86f65c
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-31 13:38:03 -0500

    updated

--- instance.py
+++ instance.py
@@ -1,16 +1,16 @@
-from tracemalloc import Snapshot
 import boto3
-import json
 import sys
-from pprint import pprint
-from datetime import datetime, timezone
-import botocore
+import argparse
+instance_id = "i-0ae4e19b44a5fbe70"
 
+def print_ec2_tagname(instance_id):
+    ec2 = boto3.resource("ec2", region_name="us-west-1")
+    ec2instance = ec2.Instance(instance_id)
+    instancename = ''
+    for tags in ec2instance.tags:
+        if tags["Key"] == 'Name':
+            instancename = tags["Value"]
+    #return instancename
+    print (instancename)
 
-
-
-ec2 = boto3.client('ec2')
-
-instance_response = ec2.describe_instances(InstanceIds=[attachment['i-0ae4e19b44a5fbe70']])
-
-print(instance_response)
+print_ec2_tagname(instance_id)
