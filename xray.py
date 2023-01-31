ed986c975e5b49690377a1d501006fefd1aa47b9
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-01-31 11:41:08 -0500

    update

--- xray.py
+++ xray.py
@@ -1,1 +1,31 @@
+from tracemalloc import Snapshot
+import boto3
+import json
+import sys
+from pprint import pprint
+from datetime import datetime, timezone
+import botocore
 
+
+
+
+ec2 = boto3.client('ec2')
+snapshot_response = ec2.describe_snapshots(OwnerIds=['self'])
+
+for snapshot in snapshot_response['Snapshots']:
+
+    days_old = (datetime.now(timezone.utc) - snapshot['StartTime']).days
+
+
+    if days_old >= 29:
+        print ("+++++++++++++++++++++++++++")
+        print ("+")
+        print(snapshot['SnapshotId'])
+        volumeId=snapshot['VolumeId']
+        #print(volumeId)
+        print("Snapshot is ", days_old, "days old")
+
+    
+    else :
+
+        continue
