f39c933bd6de504e26e2f0e7fa87329b77f52d97
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-02-03 16:58:59 -0500

    new test

--- parser.py
+++ parser.py
@@ -3,15 +3,13 @@ class Employee :
     empCount = 0
 
 
-    def __init__(self, name, salary){
+    def __init__(self, name, salary):
 
         self.name = name
         self.salary = salary
 
         Employee.empCount += 1
     
-    }
-
     def displayCount(self):
         print("Employee Count = ", Employee.empCount)
 
@@ -19,11 +17,14 @@ class Employee :
         print("Employee: ", self.name, ", Salary: ", self.salary )
 
 
-emp1 = Employee("Sarah", "2000")
-emp2 = Employee("John", "3000")
+emp1 = Employee("Sarah", 2000)
+emp2 = Employee("John", 3000)
 
 
-emp1.displayEmployee()
-emp2.displayEmployee()
-
-print("Total Employees = ", Employee.empCount)
+emp1 = Employee("Zara", 2000)
+emp2 = Employee("Manni", 5000)
+print ("Employee.__doc__:", Employee.__doc__)
+print ("Employee.__name__:", Employee.__name__)
+print ("Employee.__module__:", Employee.__module__)
+print ("Employee.__bases__:", Employee.__bases__)
+print ("Employee.__dict__:", Employee.__dict__ )
