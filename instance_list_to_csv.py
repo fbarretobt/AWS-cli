8c452b4e137b1cae7a52589999aa0bd9d5e80a5a
fbarretobt <100601093+fbarretobt@users.noreply.github.com>
2023-02-07 14:29:02 -0500

    upadted

--- instance_list_to_csv.py
+++ instance_list_to_csv.py
@@ -23,7 +23,7 @@ def get_snapshot(region):
 
         print("Working on ", count, "of ", snapshot_count, "in ", region)
         days_old = (datetime.now(timezone.utc) - snapshot['StartTime']).days
-        print(snapshot)
+        #print(snapshot)
         snapshot_tag_info(snapshot['SnapshotId'], region, days_old, snapshot["Encrypted"], snapshot["VolumeSize"])
         
         if count == 10:
