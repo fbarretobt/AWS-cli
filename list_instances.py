f71a16cf2dc8bc33978d38e11b838dcc0d076c13
Felipe Barreto <fbarreto@beyondtrust.com>
2023-02-08 13:26:27 -0500

    ...

--- list_instances.py
+++ list_instances.py
@@ -34,7 +34,8 @@ def list_instances(region):
 
             instance = ec2_resource.Instance(instanceID)
             volumes = instance.volumes.all()
-            print (volumes)
+            for v in volumes:
+                print(v.id)
 
             #print(name, product, instanceID, version, rootdevice, nonrootdevice)
 
