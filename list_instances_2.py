61898944406fadd712cf5a9c7fd5f063ae8df92e
Felipe Barreto <fbarreto@beyondtrust.com>
2023-02-09 10:29:27 -0500

    ...

--- list_instances_2.py
+++ list_instances_2.py
@@ -17,3 +17,31 @@ def list_snapshots(region):
 
     for instance in instaces_list:
         print(instance)
+
+
+
+##################################################################################
+### define whihch region to use or loop all the regions 
+def region(region):
+
+    if region == "all":
+        e = boto3.client('ec2')
+        regions_list = e.describe_regions()
+        regions = regions_list["Regions"]
+        region_count=len(regions)
+        count=0 
+        for region in regions:
+            count +=1 
+            print("Working on region", count, "of ", region_count, "(", region['RegionName'], ")")
+
+            list_snapshots(region['RegionName'])
+    else:
+
+        list_snapshots(region)
+
+
+##################################################################################
+### initiates the function calls  and add on terminal call for functions 
+if __name__ == '__main__':
+
+    globals()[sys.argv[1]](sys.argv[2])
