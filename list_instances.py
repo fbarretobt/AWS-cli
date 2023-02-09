ae6e3393df733752973957917fa1615304843a57
Felipe Barreto <fbarreto@beyondtrust.com>
2023-02-08 23:13:42 -0500

    ..

--- list_instances.py
+++ list_instances.py
@@ -53,7 +53,7 @@ def list_instances(region):
                     hours = int(seconds // 3600)
                     minutes = int((seconds//60)%60)
 
-
+                    rootsnapshot = ""
 
                     if hours < 4 :
                         
